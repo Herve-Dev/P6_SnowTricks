144bc565083649b01cdb6f50d03adaeb09be9ff8
Hervé Taillefond <madinina972represent@gmail.com>
2023-05-18 22:45:30 +0200

    create CommentFormType

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -6,13 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/comment', name: 'comment')]
 class CommentController extends AbstractController
 {
-    #[Route('/comment', name: 'app_comment')]
+    #[Route('/', name: 'read_comment')]
     public function index(): Response
     {
         return $this->render('comment/index.html.twig', [
             'controller_name' => 'CommentController',
         ]);
     }
+
+    #[Route('/addComment', name: 'add_comment')]
+    public function addComment(): Response
+    {
+        return $this->render('comment/index.html.twig', [
+            'controller_name' => 'CommentController',
+        ]);
+    }
+
+
 }
