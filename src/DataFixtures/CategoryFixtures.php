52704a8ff19b0d26acdf3133296598cb46139c25
Hervé Taillefond <madinina972represent@gmail.com>
2023-06-08 00:15:26 +0200

    add faker for category and tricks

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -2,16 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class CategoryFixtures extends Fixture
 {
+    private $counter = 1;
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->createCategory('Butters', $manager);
+        $this->createCategory('Grabs', $manager);
+        $this->createCategory('Spins', $manager);
+        $this->createCategory('Flips', $manager);
 
         $manager->flush();
     }
+
+    public function createCategory(string $name, ObjectManager $manager)
+    {
+        $category = new Category();
+        $category->setCategoryTricks($name);
+        $manager->persist($category);
+
+        $this->addReference('cat-'.$this->counter, $category);
+        $this->counter++;
+
+        return $category;
+    }
 }
+
--- src/DataFixtures/TricksFixtures.php
+++ src/DataFixtures/TricksFixtures.php
@@ -2,15 +2,38 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Tricks;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
 
 class TricksFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $tricksArrayName = [
+            '50-50',
+            'FrontSide 180',
+            'rails',
+            'Backside 360',
+            'Indy Grab',
+            'Nose Grab',
+            'Backflip',
+            'Cork',
+            'Tail Grab',
+            'Jib'
+        ]; 
+
+        $faker = Faker\Factory::create('fr_FR');
+        for ($trk=1; $trk <= 10 ; $trk++) { 
+            $tricks = new Tricks();
+            $tricks->setTricksName($tricksArrayName[$trk -1]);
+            $tricks->setTricksDescription($faker->text(200));
+
+            //On va chercher une référence de catégorie
+            $category = $this->getReference('cat-' . rand(1, 4));
+            $tricks->setCategory($category);
+        }
 
         $manager->flush();
     }
