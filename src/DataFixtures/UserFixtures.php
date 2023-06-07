5aba4cfbb1c88f8fe5dd552bed364b00792ec1a5
Hervé Taillefond <madinina972represent@gmail.com>
2023-06-07 22:50:10 +0200

    create branch dataFixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $passwordEncoder){}
+    
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User;
+        $user->setEmail('test@test.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword(
+            $this->passwordEncoder->hashPassword($user, 'DemoPassword123')
+        );
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
