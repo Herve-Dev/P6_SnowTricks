59b4c06b4b6a60fe772c02b5fad4f116bc563563
Hervé Taillefond <madinina972represent@gmail.com>
2023-06-08 15:36:12 +0200

    add fixture for comment and video fixtures

--- src/DataFixtures/CommentFixtures.php
+++ src/DataFixtures/CommentFixtures.php
@@ -2,16 +2,50 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Comment;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
 
-class CommentFixtures extends Fixture
+class CommentFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        
+        $faker = Faker\Factory::create('fr_FR');
+
+
+        for ($com=1; $com < 100; $com++) { 
+            $comment = new Comment();
+            $comment->setCommentTricks($faker->text(255));
+
+            $dateTime = $faker->dateTimeThisYear();
+            $dateImmutable = \DateTimeImmutable::createFromMutable($dateTime);
+
+            $comment->setCommentCreatedAt($dateImmutable);
+
+            //On va chercher la ref d'un User
+            $user = $this->getReference('usr-' . rand(1, 5));
+            $comment->setUser($user);
+
+            //On va chercher la ref d'un tricks
+            $tricks = $this->getReference('trk-' . rand(1, 10));
+            $comment->setTricks($tricks);
+
+            $manager->persist($comment);
+        }
 
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            UserFixtures::class,
+            TricksFixtures::class,
+            MediaTricksFixtures::class,
+            VideoTricksFixtures::class
+        ];
     }
 }
--- src/DataFixtures/VideoTricksFixtures.php
+++ src/DataFixtures/VideoTricksFixtures.php
@@ -2,16 +2,45 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\VideoTricks;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class VideoTricksFixtures extends Fixture
+
+class VideoTricksFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        $videoUrl = [
+            'https://www.youtube.com/embed/kOyCsY4rBH0',
+            'https://www.youtube.com/embed/9Bhh28m_yBM',
+            'https://www.youtube.com/embed/BqlxmzTJ5jo',
+            'https://www.youtube.com/embed/HJiAYHGQApU',
+            'https://www.youtube.com/embed/IlUy9gQJoss',
+        ];
+
+        for ($vidTrk=1; $vidTrk < 20; $vidTrk++) { 
+            $videoTricks = new VideoTricks();
+            $randomKey = array_rand($videoUrl);
+            $randomUrl = $videoUrl[$randomKey];
+            $videoTricks->setVideoUrl($randomUrl);
+            
+            $tricks = $this->getReference('trk-' . rand(1, 10));
+            $videoTricks->setTricks($tricks);
+
+            $manager->persist($videoTricks);
+
+        }
 
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            TricksFixtures::class
+        ];
     }
 }
